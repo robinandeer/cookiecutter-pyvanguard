2c23f13634e704add4dd88ad44bb5a4b6108b395
Robin Andeer <robin.andeer@gmail.com>
2014-07-15 10:08:36 +0200

    adds __main__ module for CLI entry point (see setup.py)

---
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The main entry point. Invoke as `{{ cookiecutter.repo_name }}' or
+`python -m {{ cookiecutter.repo_name }}'.
+"""
+from __future__ import absolute_import, unicode_literals
+import sys
+
+
+def cli():
+  """Add something useful to this function or import another version
+  of it from a submodule.
+  """
+  print('{{ cookiecutter.project_name }} CLI not implemented yet')
+
+
+if __name__ == '__main__':
+  # Exit using whatever exit code the CLI returned
+  sys.exit(cli())
