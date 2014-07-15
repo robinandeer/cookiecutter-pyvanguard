dda812e57fbe1346336316d2cee96580c95a9dba
Robin Andeer <robin.andeer@gmail.com>
2014-07-15 09:41:46 +0200

    updates tests structure to py.test compatible adds fixtures folder

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -1,28 +1,22 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 """
 test_{{ cookiecutter.repo_name }}
 ----------------------------------
 
 Tests for `{{ cookiecutter.repo_name }}` module.
 """
-
-import unittest
-
-from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
+import pytest
+import {{ cookiecutter.repo_name }}
 
 
-class Test{{ cookiecutter.repo_name|capitalize }}(unittest.TestCase):
+class Test{{ cookiecutter.repo_name|capitalize }}(object):
+  @classmethod
+  def set_up(self):
+    pass
 
-    def setUp(self):
-        pass
+  def test_something(self):
+    pass
 
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+  @classmethod
+  def tear_down(self):
+    pass
